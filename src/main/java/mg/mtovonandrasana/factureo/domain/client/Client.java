2eb220a0ddfa8487fbffd5747347781414512002
Michaël Tovonandrasana <ndrasanamike@gmail.com>
2021-07-05 19:07:50 +0300

    fix client entity

--- src/main/java/mg/mtovonandrasana/factureo/domain/client/Client.java
+++ src/main/java/mg/mtovonandrasana/factureo/domain/client/Client.java
@@ -22,34 +22,27 @@ public class Client {
     private String raisonSocial;
     @NotBlank
     private String reference;
-
-    @NotNull
-    @OneToOne
-    @MapsId
-    private Adresse headOfficeAddress;
+    @NotBlank
+    private String headOfficeAddress;
+    @NotBlank
+    private Integer postalCode;
+    @NotBlank
+    private String city;
+    private String country = "Madagascar";
 
 
     public Client() {
     }
 
-    public Client(String raisonSocial, 
-                    String nif, 
-                    String stat, 
-                    String reference, 
-                    Adresse headOfficeAddress) {
-        this.raisonSocial = raisonSocial;
+    public Client(String nif, String stat, String raisonSocial, String reference, String headOfficeAddress, Integer postalCode, String city, String country) {
         this.nif = nif;
         this.stat = stat;
+        this.raisonSocial = raisonSocial;
         this.reference = reference;
         this.headOfficeAddress = headOfficeAddress;
-    }
-
-    public String getRaisonSocial() {
-        return this.raisonSocial;
-    }
-
-    public void setRaisonSocial(String raisonSocial) {
-        this.raisonSocial = raisonSocial;
+        this.postalCode = postalCode;
+        this.city = city;
+        this.country = country;
     }
 
     public String getNif() {
@@ -68,6 +61,14 @@ public class Client {
         this.stat = stat;
     }
 
+    public String getRaisonSocial() {
+        return this.raisonSocial;
+    }
+
+    public void setRaisonSocial(String raisonSocial) {
+        this.raisonSocial = raisonSocial;
+    }
+
     public String getReference() {
         return this.reference;
     }
@@ -76,17 +77,36 @@ public class Client {
         this.reference = reference;
     }
 
-    public Adresse getHeadOfficeAddress() {
+    public String getHeadOfficeAddress() {
         return this.headOfficeAddress;
     }
 
-    public void setHeadOfficeAddress(Adresse headOfficeAddress) {
+    public void setHeadOfficeAddress(String headOfficeAddress) {
         this.headOfficeAddress = headOfficeAddress;
     }
 
-    public Client raisonSocial(String raisonSocial) {
-        setRaisonSocial(raisonSocial);
-        return this;
+    public Integer getPostalCode() {
+        return this.postalCode;
+    }
+
+    public void setPostalCode(Integer postalCode) {
+        this.postalCode = postalCode;
+    }
+
+    public String getCity() {
+        return this.city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getCountry() {
+        return this.country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
     }
 
     public Client nif(String nif) {
@@ -99,13 +119,33 @@ public class Client {
         return this;
     }
 
+    public Client raisonSocial(String raisonSocial) {
+        setRaisonSocial(raisonSocial);
+        return this;
+    }
+
     public Client reference(String reference) {
         setReference(reference);
         return this;
     }
 
-    public Client headOfficeAddress(Adresse headOfficeAddress) {
+    public Client headOfficeAddress(String headOfficeAddress) {
         setHeadOfficeAddress(headOfficeAddress);
+        return this;
+    }
+
+    public Client postalCode(Integer postalCode) {
+        setPostalCode(postalCode);
+        return this;
+    }
+
+    public Client city(String city) {
+        setCity(city);
+        return this;
+    }
+
+    public Client country(String country) {
+        setCountry(country);
         return this;
     }
 
@@ -117,27 +157,31 @@ public class Client {
             return false;
         }
         var client = (Client) o;
-        return Objects.equals(raisonSocial, client.raisonSocial) 
-                && Objects.equals(nif, client.nif) 
-                && Objects.equals(stat, client.stat) 
-                && Objects.equals(reference, client.reference) 
-                && Objects.equals(headOfficeAddress, client.headOfficeAddress);
+        return Objects.equals(nif, client.nif) && Objects.equals(stat, client.stat)
+                && Objects.equals(raisonSocial, client.raisonSocial) && Objects.equals(reference, client.reference)
+                && Objects.equals(headOfficeAddress, client.headOfficeAddress)
+                && Objects.equals(postalCode, client.postalCode) && Objects.equals(city, client.city)
+                && Objects.equals(country, client.country);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(raisonSocial, nif, stat, reference, headOfficeAddress);
+        return Objects.hash(nif, stat, raisonSocial, reference, headOfficeAddress, postalCode, city, country);
     }
 
     @Override
     public String toString() {
         return "{" +
-            " raisonSocial='" + getRaisonSocial() + "'" +
-            ", nif='" + getNif() + "'" +
+            " nif='" + getNif() + "'" +
             ", stat='" + getStat() + "'" +
+            ", raisonSocial='" + getRaisonSocial() + "'" +
             ", reference='" + getReference() + "'" +
             ", headOfficeAddress='" + getHeadOfficeAddress() + "'" +
+            ", postalCode='" + getPostalCode() + "'" +
+            ", city='" + getCity() + "'" +
+            ", country='" + getCountry() + "'" +
             "}";
     }
+
 
 }
